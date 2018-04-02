8bb67b22144e3091882778bc0ec1ac1948eb611a
hockey_for_NOI <heqi961102@163.com>
2018-04-02 05:09:05 +0000

    Add ServerStorage module.

---
+++ include/server_storage.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <mutex>
+#include <string>
+#include <fstream>
+#include <memory>
+#include <unordered_set>
+
+namespace	oi
+{
+
+class	PackedInput;
+class	PackedOutput;
+class	ServerStorage;
+
+class	PackedInput : public std::unique_ptr<std::ifstream>
+{
+public:
+	PackedInput() = delete;
+private:
+	PackedInput(std::string const& filename);
+	~PackedInput();
+	std::string	m_filename;
+};
+
+class	PackedOutput : public std::unique_ptr<std::ofstream>
+{
+public:
+	PackedOutput() = delete;
+private:
+	PackedOutput(std::string const& filename);
+	~PackedOutput();
+	std::string	m_filename;
+};
+
+class	ServerStorage
+{
+public:
+	static	std::string	storage_root;
+	static	ServerStorage*	get_instance();
+
+	PackedInput	read(std::string const& filename);
+	PackedOutput	write(std::string const& filename);
+	void	close(std::string const& filename);
+private:
+	ServerStorage();
+	static	ServerStorage*	m_instance;
+	static	std::mutex	m_mutex;
+	std::unordered_set <std::string>	m_usage;
+};
+
+}	// end namespace oi
